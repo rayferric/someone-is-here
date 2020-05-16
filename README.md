f17c1657fc2bcc769ffa562303c8be8ef9b24098
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2020-05-17 01:48:19 +0200

    Upload files 📤

---
+++ README.md
@@ -0,0 +1,61 @@
+[![](logo.png)](https://youtu.be/BeG5FqTpl9U)
+
+# Someone is Here
+
+👌 A Discord bot that brings back the features of April Fools' 2018.
+
+[![](https://img.shields.io/github/license/rayferric/someone-is-here?style=for-the-badge)](LICENSE)
+[![](https://img.shields.io/badge/invite-Discord-blue?style=for-the-badge)](https://discord.com/api/oauth2/authorize?client_id=709969798620119092&permissions=8&scope=bot)
+
+## Features
+
+### Roll the Dice
+
+- 🎲 **Randomly select someone at random** with `@someone`.
+- 🦊 **While in a voice channel, typing** `OwO` in any text channel causes Discord to let out a cry of distress.
+- 📢 `@supereveryone` **is here!** You can now bypass `@everyone` permissions for when you need to say something urgent or important.
+
+## Getting Started
+
+### Installing
+
+🔰 **[Invite](https://discord.com/api/oauth2/authorize?client_id=709969798620119092&permissions=8&scope=bot)** the bot to your guild.
+
+## Development
+
+### Prerequisites
+
+- ⚙️ Visual C++ **[Build Tools](https://visualstudio.microsoft.com/pl/visual-cpp-build-tools)**
+- 🐍 Latest Version of **[Python](https://www.python.org)**
+- 💻 **[Node.js](https://nodejs.org)** Runtime
+- 🔗 **[Git](https://git-scm.com)** Version Control System
+- 🔊 **[FFmpeg](https://www.ffmpeg.org)** Multimedia Framework
+- 🤖 Discord **[Application](https://discord.com/developers/applications)**
+
+### Setup
+
+💻 Run the following commands in your terminal:
+
+```bash
+git clone https://github.com/rayferric/someone-is-here.git
+cd someone-is-here
+npm install
+```
+
+📝 Replace `MyJu1cy-t0k3n` inside `package.json` with your personal bot token.
+
+### Running
+
+```bash
+npm test
+```
+
+## About
+
+### Authors
+
+- Ray Ferric (**[rayferric](https://github.com/rayferric)**)
+
+### License
+
+This project is licensed under the MIT License. See the **[LICENSE](LICENSE)** file for details.
---
+++ index.js
@@ -0,0 +1,124 @@
+const Discord = require('discord.js');
+const http = require('http');
+
+const maxMentionsPerRequest = 4;
+const maxVoiceJobCount = 16;
+
+const voiceJobGuilds = [];
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setActivity('with @someone').catch(e => console.log(e)); // Playing with @someone
+});
+
+client.on('guildCreate', guild => {
+    console.log(`Joined new guild: ${guild.name} <${guild.id}> (${guild.memberCount} members).`);
+
+    guild.roles.fetch().then(roles => {
+        if(roles.cache.find(role => role.name === 'supereveryone') != null)return;
+        guild.roles.create({ data: { name: 'supereveryone' } });
+        log(guild, 'Created new @supereveryone role.');
+    });
+
+    if(guild.systemChannel == null)return;
+    setTimeout(() => guild.systemChannel.send('https://youtu.be/BeG5FqTpl9U').then(() => {
+        guild.systemChannel.send({
+            embed: {
+                color: 0x738ad6,
+                title: 'Someone is Here',
+                author: { name: client.user.username, icon_url: client.user.avatarURL() },
+                description: "👌 A Discord bot that brings back the features of April Fools' 2018.",
+                fields: [
+                    { name: 'Features', value: ' • 🎲 Randomly select someone at random with `@someone`.\n • 🦊 While in a voice channel, typing OwO in any text channel causes Discord to let out a cry of distress.\n • 📢 `@supereveryone` is here! You can now bypass `@everyone` permissions for when you need to say something urgent or important.' },
+                    { name: 'GitHub', value: '🐙 https://github.com/rayferric/someone-is-here' }
+                ]
+            }
+        }).catch(e => console.log(e));
+    }).catch(e => console.log(e)), 1000);
+});
+
+client.on('message', message => {
+    parseSomeone(message);
+    parseOwo(message);
+    parseSupereveryone(message);
+}); 
+
+console.log(`Logging in using token: ${process.env.TOKEN}...`),
+
+client.login(process.env.TOKEN).then(
+    () => console.log(`Logged in as ${client.user.tag}.`),
+    () => {
+        console.log('Failed to log in, check your internet connection and validate the bot token before trying again.');
+        process.exit();
+    }
+);
+
+// Glitch specific code to keep the project online 24/7
+
+http.createServer((reqest, result) => {
+    result.writeHead(200, {'Content-Type': 'text/plain'});
+    result.write('Bot status has been refreshed.');
+    result.end();
+    console.log('Bot status has been refreshed.');
+}).listen(process.env.PORT);
+
+setInterval(() => http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me`), 280000);
+
+// End of Glitch specific code
+
+const parseSomeone = (message) => {
+    let count = message.cleanContent.split('@someone').length - 1;
+    if(count < 1)return
+    count = Math.min(count, maxMentionsPerRequest);
+    
+    message.guild.members.fetch().then(members => {
+        const randomMembers = members.filter(member => !member.user.bot && member.user.id !== message.author.id).random(count);
+        for(const randomMember of randomMembers) {
+            if(randomMember == null)break;
+
+            message.channel.send(randomMember.user.toString()).then(message => message.delete()).catch(e => console.log(e));
+            log(message.guild, `Mentioned ${randomMember.user.tag} <${randomMember.user.id}> in reply to ${message.author.tag} <${message.author.id}>.`);
+        }
+    }).catch(e => console.log(e));
+}
+
+const parseOwo = (message) => {
+    const voiceChannel = message.member.voice.channel;
+    if(voiceChannel == null)return;
+
+    const isOwo = message.cleanContent.toLowerCase().split(/[^A-Za-z]/).includes('owo');
+    if(!isOwo)return;
+
+    if(voiceJobGuilds.length >= maxVoiceJobCount) {
+        log(message.guild, 'Refused to stream audio - all voice jobs are active.');
+        return;
+    }
+    if(voiceJobGuilds.includes(message.guild)) {
+        log(message.guild, 'Refused to stream audio - already playing in this guild.');
+        return;
+    }
+    voiceJobGuilds.push(message.guild);
+
+    log(message.guild, 'Started streaming audio.');
+    voiceChannel.join().then(connection => {
+        const dispatcher = connection.play('owo.mp3');
+        dispatcher.on('speaking', (speaking) => {
+            if(!speaking)voiceChannel.leave()
+        });
+        connection.on('disconnect', () => {
+            log(message.guild, 'Finished streaming audio.');
+            voiceJobGuilds.splice(voiceJobGuilds.indexOf(message.guild), 1);
+        });
+    }).catch(e => console.log(e));
+}
+
+const parseSupereveryone = (message) => {
+    if(!message.cleanContent.includes('@supereveryone'))return;
+    message.channel.send('@everyone').then(message => message.delete()).catch(e => console.log(e));
+    log(message.guild, `Mentioned @everyone in reply to ${message.author.tag} <${message.author.id}>.`);
+}
+
+const log = (guild, text) => {
+    console.log(`[${guild.name} <${guild.id}>]: ${text}`);
+}
